eda8ed70d1ca1222feddc919258ece43b420c78c
LeBronidas <leonidaslaufer9@gmail.com>
2025-11-12 22:39:30 -0300

    Implementa menu e mais
    -Menu simples(Novo Jogo, Ranking e Sair)
    -Opção Ranking ainda não foi implementada
    -Colisão do jogador com um inimigo diminui a vida em 1
    -Jogador morre quando vida chega a zero e programa exibe tela de morte
    -Projeto dividido em outros arquivos(Funções, Structs, Defines)

--- main.c
+++ main.c
@@ -1,50 +1,7 @@
+#include "Funcoes.h"
 #include "raylib.h"
 #include <stdlib.h>
 #include <stdbool.h>
-
-typedef struct Jogador {
-	int x;
-	int y;
-	int largura;
-	int altura;
-	int vidas;
-} Jogador;
-
-typedef struct tiro {
-	int x;
-	int y;
-	bool ativo;
-} Tiro;
-
-typedef struct inimigo {
-	int x;
-	int y;
-	int largura;
-	int altura;
-	bool ativo;
-} Inimigo;
-
-typedef struct Spawnpoint {
-	int x;
-	int y;
-} Spawnpoint;
-
-#define VEL_JOGADOR 7
-#define LARG_JOGADOR 20
-#define ALT_JOGADOR 20
-
-#define LARG_INIMIGO 20
-#define ALT_INIMIGO 20
-#define VEL_INIMIGO 1
-#define MAX_INIMIGOS 5
-
-#define LARG_TIRO 5
-#define ALT_TIRO 10
-#define VEL_TIRO 8
-#define MAX_TIROS 8
-
-#define LARGURA 600
-#define ALTURA 600
 
 Spawnpoint teste_1[MAX_INIMIGOS] =
 {
@@ -55,22 +12,15 @@ Spawnpoint teste_1[MAX_INIMIGOS] =
   {500, -50}
 };
 
-void inicializaTiros(Tiro* tiros);
-void atira(Tiro* tiros, Jogador jogador);
-void atualizaTiros(Tiro* tiros);
-void desenhaTiros(Tiro* tiros);
-
-void spawnaInimigos(Inimigo* inimigos, Spawnpoint* mapa);
-void atualizaInimigos(Inimigo* inimigos);
-void desenhaInimigos(Inimigo* inimigos);
-
-void checaColisoes(Tiro* tiros, Inimigo* inimigos);
-
 
 int main() {
 	Jogador jogador = { LARGURA / 2, ALTURA / 2 , LARG_JOGADOR, LARG_JOGADOR, 1 }; //Cria jogador
 	Tiro tiros[MAX_TIROS]; //Cria array de tiros
 	Inimigo inimigos[MAX_INIMIGOS];
+
+	bool jogoAtivo = false;
+	bool sair = false;
+	int op = 0; // Operador do menu
 
 	InitWindow(LARGURA, ALTURA, "Teclas"); //Inicializa janela, com certo tamanho e titulo
 	SetTargetFPS(60);// Ajusta a janela para 60 frames por segundo
@@ -78,156 +28,86 @@ int main() {
 	spawnaInimigos(inimigos, teste_1); // Inicializa inimigos
 
 
-	//Este laco repete enquanto a janela nao for fechada
-	//Utilizamos ele para atualizar o estado do programa / jogo
-	while (!WindowShouldClose())
+	while (!WindowShouldClose() && !sair) // Enquanto o ESC e o botão de sair nao forem pressionados
 	{
-		atualizaTiros(tiros); // Atualiza posição e estado dos tiros a cada iteração
-		atualizaInimigos(inimigos); // Atualiza posição e estado dos inimigos a cada iteração
-		checaColisoes(tiros, inimigos); // Checa colisões entre tiros e inimigos
-
-
-		if (IsKeyDown(KEY_RIGHT) || IsKeyDown(68)) // Movimento para a direita
+		if (!jogoAtivo) // Se o jogo nao comecou
 		{
-			jogador.x += VEL_JOGADOR;
-			if (jogador.x > LARGURA - jogador.largura)
+			if ((IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) && op != 0)
 			{
-				jogador.x = LARGURA - jogador.largura;
+				op--; // Move o cursor para cima caso nao esteja no topo
 			}
-		}
-		if (IsKeyDown(KEY_LEFT) || IsKeyDown(65)) // Movimento para a esquerda
-		{
-			jogador.x -= VEL_JOGADOR;
-			if (jogador.x < 0)
+			if ((IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) && op != 2)
 			{
-				jogador.x = 0;
+				op++; // Move o cursor para baixa caso nao esteja no fundo
 			}
-		}
+			if (IsKeyPressed(KEY_ENTER) && op == 0)
+			{
+				jogoAtivo = true; // Começa o jogo caso ENTER tenha sido pressioando, e caso cursor esteja em Novo Jogo
+			}
+			if (IsKeyPressed(KEY_ENTER) && op == 2)
+			{
+				sair = true; // Sai caso ENTER tenha sido pressionado, e caso cursor esteja em Sair
+			}
 
-		if (IsKeyPressed(32)) // Atira se barra de espaço for pressionada
+
+			BeginDrawing();
+			ClearBackground(SKYBLUE);
+			menu(op); // Desenha o menu
+			EndDrawing();
+		}
+		else
 		{
-			atira(tiros, jogador);
-		}
+			if (jogador.vidas != 0) // Caso jogo tenha começado
+			{
+				atualizaTiros(tiros); // Atualiza posição e estado dos tiros a cada iteração
+				atualizaInimigos(inimigos); // Atualiza posição e estado dos inimigos a cada iteração
+				checaColisoesTiro(tiros, inimigos); // Checa colisões entre tiros e inimigos
+				checaColisoesJogador(&jogador, inimigos); // Checa colisões entre jogador e inimigos
 
-		// Atualiza o que eh mostrado na tela a partir do estado do jogo
-		BeginDrawing(); //Inicia o ambiente de desenho na tela
-		ClearBackground(RAYWHITE); //Limpa a tela e define cor de fundo
-		DrawRectangle(jogador.x, jogador.y, jogador.largura, jogador.altura, BLUE); //Desenha o jogador na tela
-		desenhaTiros(tiros); // Desenha o tiro na tela
-		desenhaInimigos(inimigos); // Desenha os inimigos na tela
-		EndDrawing(); //Finaliza o ambiente de desenho na tela
+
+				if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) // Movimento para a direita
+				{
+					jogador.x += VEL_JOGADOR;
+					if (jogador.x > LARGURA - jogador.largura)
+					{
+						jogador.x = LARGURA - jogador.largura;
+					}
+				}
+				if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) // Movimento para a esquerda
+				{
+					jogador.x -= VEL_JOGADOR;
+					if (jogador.x < 0)
+					{
+						jogador.x = 0;
+					}
+				}
+
+				if (IsKeyPressed(KEY_SPACE)) // Atira se barra de espaço for pressionada
+				{
+					atira(tiros, jogador);
+				}
+
+				
+				BeginDrawing(); 
+				ClearBackground(RAYWHITE); 
+				DrawRectangle(jogador.x, jogador.y, jogador.largura, jogador.altura, BLUE); //Desenha o jogador na tela
+				desenhaTiros(tiros); // Desenha o tiro na tela
+				desenhaInimigos(inimigos); // Desenha os inimigos na tela
+				EndDrawing(); 
+			}
+
+
+			else
+			{
+				BeginDrawing();
+				ClearBackground(BLACK);
+				DrawText("TU MORREU", LARGURA / 4, ALTURA / 2, 50, RED); // Desenha tela de morte
+				EndDrawing();
+			}
+					 		
+		}
 	}
 
 	CloseWindow(); // Fecha a janela
 	return 0;
-}
-
-void inicializaTiros(Tiro* tiros)
-{
-	for (int i = 0; i < MAX_TIROS; i++) {
-		tiros[i].ativo = false; // Inativa todos os tiros
-	}
-}
-
-void atira(Tiro* tiros, Jogador jogador)
-{
-	for (int i = 0; i < MAX_TIROS; i++)
-	{
-		if (!tiros[i].ativo)
-		{
-			tiros[i].ativo = true; // Ativa tiro
-			tiros[i].x = jogador.x - LARG_TIRO / 2 + LARG_JOGADOR / 2; // Centraliza tiro com jogador
-			tiros[i].y = jogador.y - ALT_JOGADOR / 2; // Ajusta altura para sair acima do jogador
-			break;
-		}
-	}
-}
-
-void atualizaTiros(Tiro* tiros)
-{
-	for (int i = 0; i < MAX_TIROS; i++)
-	{
-		if (tiros[i].ativo)
-		{
-			if (tiros[i].y < 0)
-			{
-				tiros[i].ativo = false; // Desativa tiro caso esteja fora da tela
-			}
-			tiros[i].y -= VEL_TIRO; // Atualiza posição do tiro
-		}
-	}
-}
-
-void desenhaTiros(Tiro* tiros)
-{
-	for (int i = 0; i < MAX_TIROS; i++)
-	{
-		if (tiros[i].ativo)
-		{
-			DrawRectangle(tiros[i].x, tiros[i].y, LARG_TIRO, ALT_TIRO, RED); // Desenha o tiro caso ativo
-		}
-	}
-}
-
-
-void spawnaInimigos(Inimigo* inimigos, Spawnpoint* mapa)
-{
-	for (int i = 0; i < MAX_INIMIGOS; i++)
-	{
-		inimigos[i].ativo = true; // Ativa inimigo
-		inimigos[i].largura = LARG_INIMIGO; // Define largura do inimigo
-		inimigos[i].altura = ALT_INIMIGO; // Define altura do inimigo
-		inimigos[i].x = mapa[i].x;
-		inimigos[i].y = mapa[i].y;
-	}
-}
-
-void atualizaInimigos(Inimigo* inimigos)
-{
-	for (int i = 0; i < MAX_INIMIGOS; i++)
-	{
-		if (inimigos[i].ativo)
-		{
-			inimigos[i].y += VEL_INIMIGO; // Atualiza posição do inimigo
-			if (inimigos[i].y > ALTURA)
-			{
-				inimigos[i].ativo = false; // Desativa inimigo caso esteja fora da tela
-			}
-		}
-	}
-}
-
-void desenhaInimigos(Inimigo* inimigos)
-{
-	for (int i = 0; i < MAX_INIMIGOS; i++)
-	{
-		if (inimigos[i].ativo)
-		{
-			DrawRectangle(inimigos[i].x, inimigos[i].y, inimigos[i].largura, inimigos[i].altura, GREEN); // Desenha o inimigo caso ativo
-		}
-	}
-}
-
-void checaColisoes(Tiro* tiros, Inimigo* inimigos)
-{
-	for (int i = 0; i < MAX_TIROS; i++)
-	{
-		if (tiros[i].ativo)
-		{
-			for (int j = 0; j < MAX_INIMIGOS; j++)
-			{
-				if (inimigos[j].ativo)
-				{
-					Rectangle r_Tiro = { tiros[i].x, tiros[i].y, LARG_TIRO, ALT_TIRO };
-					Rectangle r_Inimigo = { inimigos[j].x, inimigos[j].y, inimigos[j].largura, inimigos[j].altura };
-					if (CheckCollisionRecs(r_Tiro, r_Inimigo))
-					{
-						tiros[i].ativo = false;
-						inimigos[j].ativo = false;
-						break;
-					}
-				}
-			}
-		}
-	}
 }
